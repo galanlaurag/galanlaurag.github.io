54563a74388999bb57e00a76424af6a0652e19de
galanlaurag <78027413+galanlaurag@users.noreply.github.com>
2021-04-17 14:17:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function manageSound() {
-  
+var btn = document.querySelector(".mode");
+var currentMode = localStorage.getItem("mode");
+
+
+if (currentMode == "dark") {
+  // ...then use the .dark-theme class
+  document.body.classList.add("dark-mode");
+}
+
+function myFunction() {
+  document.body.classList.toggle("dark-mode");
+  let mode = "light";
+
+  if (document.body.classList.contains("dark-mode")) {
+    mode = "dark";
+  }
+  localStorage.setItem("mode", mode);
 }
